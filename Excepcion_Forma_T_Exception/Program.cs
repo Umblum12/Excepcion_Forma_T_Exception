bac93ee27bb7f7db3ddac8cabad0f192addfd4f2
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:41:30 -0500

    Initial commit

---
+++ Excepcion_Forma_T_Exception/Program.cs
@@ -0,0 +1,26 @@
+﻿using Excepcion_Forma_T_Exception;
+
+namespace Excepcion_Forma_T_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUno = 0, valorDos = 0, resultado;
+
+            Console.Write("Numero 1: ");
+            valorUno = int.Parse(Console.ReadLine());
+            Console.Write("Numero 2: ");
+            valorDos = int.Parse(Console.ReadLine());
+
+            try
+            {
+                resultado = valorUno / valorDos;
+                Console.WriteLine(resultado);
+            }  catch(DivideByZeroException dbzex)
+            {
+                Console.WriteLine(dbzex.Message);
+            }
+    }
+}
+}
